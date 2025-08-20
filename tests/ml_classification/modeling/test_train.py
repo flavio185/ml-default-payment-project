74dd63f76c52f6872ed65463f3284baf589b4aa1
flavio185 <flavio185@gmail.com>
2025-08-20 00:57:47 -0300

    fix tests

--- tests/ml_classification/modeling/test_train.py
+++ tests/ml_classification/modeling/test_train.py
@@ -1,15 +1,15 @@
 import pytest
-from ml_classification.modeling import train
+from ml_classification.modeling.train import main as train
 import pandas as pd
 
 def test_main_runs(tmp_path):
     # Create a dummy dataset
     df = pd.DataFrame({
-        'default': [0, 1, 0, 1],
+        'default_payment_next_month': [0, 1, 0, 1],
         'feature1': [1, 2, 3, 4],
         'feature2': [5, 6, 7, 8]
     })
     features_path = tmp_path/"dataset.csv"
-    df.to_csv(features_path, index=False)
-    train.main(features_path=features_path)
+    df.to_parquet(features_path, index=False)
+    train(input_path=features_path)
     # No assertion, just check code runs
